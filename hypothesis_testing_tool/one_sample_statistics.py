40be0b2db1177e668f121c8d32c694268fca80f0
alex vamv <vamvas@outlook.com.gr>
2024-07-20 19:52:29 +0100

    Adding api to calculate p-value and statistic for one sample hypothesis testing for the mean

---
+++ hypothesis_testing_tool/one_sample_statistics.py
@@ -0,0 +1,30 @@
+from scipy.stats import ttest_1samp
+
+
+class OneSampleHypothesisTesting:
+    def __init__(self, data: list, population_mean: float, alternative: str = "two-sided", alpha: float = 0.05):
+
+        if len(data) < 2:
+            raise Exception("Input data must have at least two data points")
+
+        elif len(set(data)) == 1:
+            raise Exception("Input data must not have identical elements")
+
+        else:
+            self.data = data
+
+        self.population_mean = population_mean
+
+        self.t_test_results = ttest_1samp(a=self.data, popmean=population_mean, alternative=alternative)
+
+    def calculate_t_statistic(self) -> float:
+
+        t_statistic = self.t_test_results.statistic
+
+        return t_statistic
+
+    def calculate_pvalue(self) -> float:
+
+        p_value = self.t_test_results.pvalue
+
+        return p_value
---
+++ tests/one_sample_statistics_test.py
@@ -0,0 +1,35 @@
+from hypothesis_testing_tool.one_sample_statistics import OneSampleHypothesisTesting
+import pytest
+
+
+def test_one_sample_t_test_returns_correct_t_statistic_and_p_value_when_input_has_length_of_two():
+
+    p_value = OneSampleHypothesisTesting(data=[1, 2], population_mean=3.5).calculate_pvalue()
+
+    assert round(p_value, 3) == 0.156
+
+
+def test_one_sample_t_test_returns_correct_p_value_when_input_has_length_higher_than_two():
+
+    p_value = OneSampleHypothesisTesting(data=[1, 2, 5, 8, 10], population_mean=3.5).calculate_pvalue()
+
+    assert round(p_value, 3) == 0.378
+
+
+def test_one_sample_t_test_returns_correct_t_statistic_when_input_has_length_higher_than_two():
+
+    t_statistic = OneSampleHypothesisTesting(data=[1, 2, 5, 8, 10], population_mean=3.5).calculate_t_statistic()
+
+    assert round(t_statistic, 3) == 0.991
+
+
+@pytest.mark.parametrize("input_data", [([]), [5]])
+def test_one_sample_t_test_raises_error_when_input_has_less_than_two_elements(input_data):
+    with pytest.raises(Exception, match="Input data must have at least two data points"):
+        OneSampleHypothesisTesting(input_data, population_mean=3.5)
+
+
+def test_one_sample_t_test_raises_error_when_input_has_identical_elements():
+    with pytest.raises(Exception, match="Input data must not have identical elements"):
+        input_data = [1] * 10
+        OneSampleHypothesisTesting(input_data, population_mean=3.5)
