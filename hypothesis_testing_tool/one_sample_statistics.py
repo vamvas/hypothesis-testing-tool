4e61713933331135ffef6023e32a91cfab17b0c8
alex vamv <vamvas@outlook.com.gr>
2024-07-24 23:55:57 +0100

    Restructured OneSampleHypothesisTesting api to return t_test result as an attribute instead of using methods for pvalue and t-statistic

--- hypothesis_testing_tool/one_sample_statistics.py
+++ hypothesis_testing_tool/one_sample_statistics.py
@@ -15,25 +15,8 @@ class OneSampleHypothesisTesting:
         elif len(set(data)) == 1:
             raise Exception("Input data must not have identical elements")
 
-        else:
-            self.data = data
-
-        self.population_mean = population_mean
-
         self.t_test_results = ttest_1samp(
-            a = self.data,
-            popmean = population_mean,
-            alternative = alternative,
+            a=data,
+            popmean=population_mean,
+            alternative=alternative,
         )
-
-    def calculate_t_statistic(self) -> float:
-
-        t_statistic = self.t_test_results.statistic
-
-        return t_statistic
-
-    def calculate_pvalue(self) -> float:
-
-        p_value = self.t_test_results.pvalue
-
-        return p_value
--- tests/one_sample_statistics_test.py
+++ tests/one_sample_statistics_test.py
@@ -4,25 +4,12 @@ from hypothesis_testing_tool.one_sample_statistics import (
 import pytest
 
 
-def test_one_sample_t_test_returns_correct_t_statistic_and_p_value_when_input_has_length_of_two():
+def test_one_sample_t_test_returns_correct_t_statistic_and_p_value_for_alternative_of_two_sided():
 
-    p_value = OneSampleHypothesisTesting(data=[1, 2], population_mean=3.5).calculate_pvalue()
+    t_test = OneSampleHypothesisTesting(data=[1, 2, 5, 8, 10], population_mean=3.5).t_test_results
 
-    assert round(p_value, 3) == 0.156
-
-
-def test_one_sample_t_test_returns_correct_p_value_when_input_has_length_higher_than_two():
-
-    p_value = OneSampleHypothesisTesting(data=[1, 2, 5, 8, 10], population_mean=3.5).calculate_pvalue()
-
-    assert round(p_value, 3) == 0.378
-
-
-def test_one_sample_t_test_returns_correct_t_statistic_when_input_has_length_higher_than_two():
-
-    t_statistic = OneSampleHypothesisTesting(data=[1, 2, 5, 8, 10], population_mean=3.5).calculate_t_statistic()
-
-    assert round(t_statistic, 3) == 0.991
+    assert round(t_test.pvalue, 3) == 0.378
+    assert round(t_test.statistic, 3) == 0.991
 
 
 @pytest.mark.parametrize("input_data", [([]), [5]])
