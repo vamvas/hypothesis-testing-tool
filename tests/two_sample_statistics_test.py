e90067dfa8e9422e94f69af5d7d12eb33e58b752
alex vamv <vamvas@outlook.com.gr>
2024-08-09 19:19:53 +0100

    Changing mock data for two samples to samples taken from normal distribution

--- tests/two_sample_statistics_test.py
+++ tests/two_sample_statistics_test.py
@@ -1,11 +1,13 @@
 from hypothesis_testing_tool.compute_stats.two_sample_statistics import TwoSampleTest
 import pytest
+import random
 
 
 @pytest.fixture(autouse=True)
 def mock_data_for_two_samples():
-    group_a = [1, 2, 5, 8, 10]
-    group_b = [e * 2 for e in group_a]
+    random.seed(42)
+    group_a = [random.gauss(mu=1, sigma=1) for _ in range(50)]
+    group_b = [random.gauss(mu=2, sigma=1) for _ in range(50)]
 
     return group_a, group_b
 
@@ -15,9 +17,9 @@ def test_two_sample_t_test_returns_correct_t_statistic_and_p_value_for_alternati
     group_a, group_b = mock_data_for_two_samples
     t_test = TwoSampleTest(group_a, group_b).t_test_results
 
-    assert round(t_test.pvalue, 3) == 0.212
-    assert round(t_test.statistic, 3) == -1.356
-    assert round(t_test.df) == 8.0
+    assert round(t_test.pvalue, 3) == 0.000
+    assert round(t_test.statistic, 3) == -7.016
+    assert t_test.df == 98.0
 
 
 def test_two_sample_t_test_returns_correct_t_statistic_and_p_value_for_alternative_of_two_sided_and_unequal_variance(mock_data_for_two_samples):
@@ -25,9 +27,9 @@ def test_two_sample_t_test_returns_correct_t_statistic_and_p_value_for_alternati
     group_a, group_b = mock_data_for_two_samples
     t_test = TwoSampleTest(group_a, group_b, equal_var=False).t_test_results
 
-    assert round(t_test.pvalue, 3) == 0.225
-    assert round(t_test.statistic, 3) == -1.356
-    assert round(t_test.df, 3) == 5.882
+    assert round(t_test.pvalue, 3) == 0.000
+    assert round(t_test.statistic, 3) == -7.016
+    assert round(t_test.df, 3) == 95.507
 
 
 def test_two_sample_t_test_returns_correct_t_statistic_and_p_value_for_alternative_of_greater_and_equal_variance(mock_data_for_two_samples):
@@ -35,9 +37,9 @@ def test_two_sample_t_test_returns_correct_t_statistic_and_p_value_for_alternati
     group_a, group_b = mock_data_for_two_samples
     t_test = TwoSampleTest(group_a, group_b, alternative="greater").t_test_results
 
-    assert round(t_test.pvalue, 3) == 0.894
-    assert round(t_test.statistic, 3) == -1.356
-    assert round(t_test.df) == 8.0
+    assert round(t_test.pvalue, 3) == 1.000
+    assert round(t_test.statistic, 3) == -7.016
+    assert t_test.df == 98.0
 
 
 def test_two_sample_t_test_returns_correct_t_statistic_and_p_value_for_alternative_of_greater_and_unequal_variance(mock_data_for_two_samples):
@@ -45,9 +47,9 @@ def test_two_sample_t_test_returns_correct_t_statistic_and_p_value_for_alternati
     group_a, group_b = mock_data_for_two_samples
     t_test = TwoSampleTest(group_a, group_b, equal_var=False, alternative="greater").t_test_results
 
-    assert round(t_test.pvalue, 3) == 0.888
-    assert round(t_test.statistic, 3) == -1.356
-    assert round(t_test.df, 3) == 5.882
+    assert round(t_test.pvalue, 3) == 1.000
+    assert round(t_test.statistic, 3) == -7.016
+    assert round(t_test.df, 3) == 95.507
 
 
 def test_two_sample_t_test_returns_correct_t_statistic_and_p_value_for_alternative_of_less_and_equal_variance(mock_data_for_two_samples):
@@ -55,9 +57,9 @@ def test_two_sample_t_test_returns_correct_t_statistic_and_p_value_for_alternati
     group_a, group_b = mock_data_for_two_samples
     t_test = TwoSampleTest(group_a, group_b, alternative="less").t_test_results
 
-    assert round(t_test.pvalue, 3) == 0.106
-    assert round(t_test.statistic, 3) == -1.356
-    assert round(t_test.df) == 8.0
+    assert round(t_test.pvalue, 3) == 0.000
+    assert round(t_test.statistic, 3) == -7.016
+    assert t_test.df == 98.0
 
 
 def test_two_sample_t_test_returns_correct_t_statistic_and_p_value_for_alternative_of_less_and_unequal_variance(mock_data_for_two_samples):
@@ -65,6 +67,6 @@ def test_two_sample_t_test_returns_correct_t_statistic_and_p_value_for_alternati
     group_a, group_b = mock_data_for_two_samples
     t_test = TwoSampleTest(group_a, group_b, equal_var=False, alternative="less").t_test_results
 
-    assert round(t_test.pvalue, 3) == 0.112
-    assert round(t_test.statistic, 3) == -1.356
-    assert round(t_test.df, 3) == 5.882
+    assert round(t_test.pvalue, 3) == 0.000
+    assert round(t_test.statistic, 3) == -7.016
+    assert round(t_test.df, 3) == 95.507
